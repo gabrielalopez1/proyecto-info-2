cb5fa6e4cb4e96f0019ad19fd0126d5f59ab93c8
gabrielalopez1 <163205409+gabrielalopez1@users.noreply.github.com>
2025-07-15 21:06:52 -0500

    modelo de pandas

---
+++ pandas_model.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import QAbstractTableModel, Qt
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, df):
+        super().__init__()
+        self._df = df
+
+    def rowCount(self, parent=None):
+        return self._df.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._df.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._df.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._df.columns[section])
+            if orientation == Qt.Vertical:
+                return str(self._df.index[section])
+        return None
